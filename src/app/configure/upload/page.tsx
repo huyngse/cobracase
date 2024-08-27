8698f90780a2c7928633ff5905a447ffa8b5fcbf
huyngse <ngogiahuy775309@gmail.com>
2024-08-27 18:42:46 +0700

    Set up prisma

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -13,6 +13,7 @@ const Page = () => {
     const [isPending, startTrasition] = useTransition();
     const [uploadProgress, setUploadProgress] = useState(0);
     const router = useRouter();
+
     const { startUpload, isUploading } = useUploadThing("imageUploader", {
         onClientUploadComplete: ([data]) => {
             const configId = data.serverData.configId;
@@ -24,6 +25,7 @@ const Page = () => {
             setUploadProgress(p);
         }
     });
+
     const onDropRejected = (rejectedFiles: FileRejection[]) => {
         const [file] = rejectedFiles;
         toast({
@@ -33,10 +35,12 @@ const Page = () => {
         });
         setIsDragOver(false);
     }
+    
     const onDropAccepted = (acceptedFiles: File[]) => {
         startUpload(acceptedFiles, { configId: undefined });
         setIsDragOver(false);
     }
+
     return (
         <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center", {
             "ring-blue-900/25 bg-blue-900/10": isDragOver
