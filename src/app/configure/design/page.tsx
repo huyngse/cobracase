c24ed321c2ee34b289f6cfe9b81ef0589d6ee4c9
huyngse <ngogiahuy775309@gmail.com>
2024-08-28 08:36:06 +0700

    Resize and drag

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,8 +1,29 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
 import React from 'react'
-
-const Page = async () => {
+import DesignConfigurator from './DesignConfigurator';
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") return notFound();
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) return notFound();
+  const { imageUrl, width, height } = configuration;
   return (
-    <div>Page</div>
+    <DesignConfigurator
+      congfigId={id}
+      imageUrl={imageUrl}
+      dimensions={{
+        width: width,
+        height: height,
+      }}
+    />
   )
 }
 
