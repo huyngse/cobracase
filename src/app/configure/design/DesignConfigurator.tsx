561e43e926127f1d24e3677f1b59f0b11a157f8b
huyngse <ngogiahuy775309@gmail.com>
2024-08-28 18:14:41 +0700

    Update crop image

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -4,7 +4,7 @@ import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { cn, formatPrice } from '@/lib/utils';
 import NextImage from 'next/image';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Rnd } from 'react-rnd';
 import { Description, Radio, RadioGroup, Label as RadioLabel } from '@headlessui/react';
 import { COLORS, FINISHES, MATERIALS, MODELS } from '@/validators/option-validator';
@@ -14,6 +14,8 @@ import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
 import { Button } from '@/components/ui/button';
 import { ArrowRight, Check, ChevronsUpDown } from 'lucide-react';
 import { BASE_PRICE } from '@/config/product';
+import { useUploadThing } from '@/lib/uploadthing';
+import { toast } from '@/components/ui/use-toast';
 interface DesignConfiguratorProps {
   congfigId: string,
   imageUrl: string,
@@ -35,13 +37,82 @@ const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfigura
     material: MATERIALS.options[0],
     finish: FINISHES.options[0],
   })
+  const [renderedDimension, setRenderedDimension] = useState({
+    width: dimensions.width / 4,
+    height: dimensions.height / 4,
+  })
+  const [renderedPosition, setRenderedPosition] = useState({
+    x: 150,
+    y: 205,
+  })
+
+  const phoneCaseRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { startUpload } = useUploadThing("imageUploader");
+  async function saveConfiguration() {
+    try {
+      const { left: caseLeft, top: caseTop, width, height } = phoneCaseRef.current!.getBoundingClientRect();
+      const { left: containerLeft, top: containerTop } = containerRef.current!.getBoundingClientRect();
+      const leftOffset = caseLeft - containerLeft;
+      const topOffset = caseTop - containerTop;
+      const actualX = renderedPosition.x - leftOffset;
+      const actualY = renderedPosition.y - topOffset;
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d");
+      const userImage = new Image();
+      userImage.crossOrigin = "anonymous";
+      userImage.src = imageUrl;
+      await new Promise((resolve) => userImage.onload = resolve);
+      ctx?.drawImage(userImage, actualX, actualY, renderedDimension.width, renderedDimension.height);
+      const base64 = canvas.toDataURL();
+      const base64Data = base64.split(',')[1];
+      const blob = base64ToBlob(base64Data, "image/png");
+      const file = new File([blob], "cropimage.png", { type: "image/png" });
+      // downloadFile(file);
+      await startUpload([file], { configId: congfigId })
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        description: "There was a problem saving your config, please try again.",
+        variant: "destructive"
+      })
+    }
+  }
+
+  // const downloadFile = (file: File) => {
+  //   const url = URL.createObjectURL(file);
+  //   const a = document.createElement('a');
+  //   a.href = url;
+  //   a.download = file.name;
+  //   document.body.appendChild(a);
+  //   a.click();
+  //   document.body.removeChild(a);
+  //   URL.revokeObjectURL(url);
+  // };
+
+  function base64ToBlob(base64: string, mimeType: string) {
+    //utf8
+    const byteCharacters = atob(base64);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: mimeType })
+  }
   return (
-    <div className='relative mt-20 grid grid-cols-3  mb-20 pb-20'>
-      <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+    <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3  mb-20 pb-20'>
+      <div
+        ref={containerRef}
+        className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'
+      >
         <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
           <AspectRatio
             ratio={896 / 1831}
             className='pointer-events-none relative z-50 w-full'
+            ref={phoneCaseRef}
           >
             <NextImage
               alt='phone image'
@@ -69,6 +140,18 @@ const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfigura
             topRight: <HandleComponent />,
             topLeft: <HandleComponent />
           }}
+          onResizeStop={(_, __, ref, ___, { x, y }) => {
+            setRenderedDimension({
+              height: parseInt(ref.style.height.slice(0, -2)),
+              width: parseInt(ref.style.width.slice(0, -2)),
+            })
+            setRenderedPosition({ x, y })
+          }}
+          onDragStop={(_, data) => {
+            const { x, y } = data;
+            setRenderedPosition({ x, y })
+
+          }}
           className='absolute z-20 border-[3px]  border-primary'
         >
           <div className='relative  w-full h-full'>
@@ -81,7 +164,7 @@ const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfigura
           </div>
         </Rnd>
       </div>
-      <div className='h-[37.5rem] flex flex-col bg-white'>
+      <div className='h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white'>
         <ScrollArea className='relative flex-1 overflow-auto'>
           <div aria-hidden="true" className='absolute z-10  inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none' />
           <div className='px-8 pb-12 pt-8'>
@@ -208,8 +291,12 @@ const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfigura
               <p className='font-medium whitespace-nowrap'>
                 {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
               </p>
-              <Button size="sm" className='w-full'>
-                Continue <ArrowRight className='w-4 h-4 ml-1.5 inline'/>
+              <Button
+                size="sm"
+                className='w-full'
+                onClick={saveConfiguration}
+              >
+                Continue <ArrowRight className='w-4 h-4 ml-1.5 inline' />
               </Button>
             </div>
           </div>
