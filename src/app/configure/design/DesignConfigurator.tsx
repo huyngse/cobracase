277077f046bb4a12efc4ae0003b7c1c1a0a57136
huyngse <ngogiahuy775309@gmail.com>
2024-08-28 15:25:13 +0700

    More design options

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -2,13 +2,18 @@
 import HandleComponent from '@/components/HandleComponent';
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { cn } from '@/lib/utils';
+import { cn, formatPrice } from '@/lib/utils';
 import NextImage from 'next/image';
 import React, { useState } from 'react';
 import { Rnd } from 'react-rnd';
-import { Radio, RadioGroup } from '@headlessui/react';
-import { COLORS } from '@/validators/option-validator';
+import { Description, Radio, RadioGroup, Label as RadioLabel } from '@headlessui/react';
+import { COLORS, FINISHES, MATERIALS, MODELS } from '@/validators/option-validator';
 import { Label } from '@/components/ui/label';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { ArrowRight, Check, ChevronsUpDown } from 'lucide-react';
+import { BASE_PRICE } from '@/config/product';
 interface DesignConfiguratorProps {
   congfigId: string,
   imageUrl: string,
@@ -20,11 +25,16 @@ interface DesignConfiguratorProps {
 const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfiguratorProps) => {
 
   const [options, setOptions] = useState<{
-    color: (typeof COLORS)[number]
+    color: (typeof COLORS)[number],
+    model: (typeof MODELS.options)[number],
+    material: (typeof MATERIALS.options)[number],
+    finish: (typeof FINISHES.options)[number],
   }>({
-    color: COLORS[0]
+    color: COLORS[0],
+    model: MODELS.options[0],
+    material: MATERIALS.options[0],
+    finish: FINISHES.options[0],
   })
-
   return (
     <div className='relative mt-20 grid grid-cols-3  mb-20 pb-20'>
       <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
@@ -78,34 +88,132 @@ const DesignConfigurator = ({ congfigId, imageUrl, dimensions }: DesignConfigura
             <h2 className='tracking-tight font-bold text-3xl'>Customize your case</h2>
             <div className='w-full h-px bg-zinc-200 my-6' />
             <div className='relative mt-4 h-full flex flex-col justify-between'>
-              <RadioGroup
-                value={options.color}
-                onChange={(val) => {
-                  setOptions((prev) => ({
-                    ...prev,
-                    color: val
-                  }))
-                }}
-              >
-                <Label>Color: {options.color.label}</Label>
-                <div className='mt-3 flex items-center space-x-3'>
-                  {
-                    COLORS.map((color) => (
-                      <Radio key={color.label} value={color}
-                        className={({ checked, focus }) => cn("relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent", {
-                          [`border-${color.tw}`]: focus || checked
-                        })}
-                      >
-                        <span className={cn(`bg-${color.tw}`, "h-8 w-8 rounded-full border border-black border-opacity-10")}/>
-                      </Radio>
-                    ))
-                  }
+              <div className='flex flex-col gap-6'>
+                <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    setOptions((prev) => ({
+                      ...prev,
+                      color: val
+                    }))
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
+                  <div className='mt-3 flex items-center space-x-3'>
+                    {
+                      COLORS.map((color) => (
+                        <Radio key={color.label} value={color}
+                          className={({ checked, focus }) => cn("relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent", {
+                            [`border-${color.tw}`]: focus || checked
+                          })}
+                        >
+                          <span className={cn(`bg-${color.tw}`, "h-8 w-8 rounded-full border border-black border-opacity-10")} />
+                        </Radio>
+                      ))
+                    }
+                  </div>
+                </RadioGroup>
+                <div className='relative flex flex-col gap-3 w-full'>
+                  <Label>Model</Label>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="outline"
+                        role='combobox'
+                        className='w-full justify-between'>
+                        {options.model.label}
+                        <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      {MODELS.options.map(model => (
+                        <DropdownMenuItem
+                          key={model.label}
+                          className={cn("flex text-sm gap-2 items-center p-1.5 cursor-default hover:bg-zinc-100", {
+                            "bg-zinc-100": model.label === options.model.label
+                          })}
+                          onClick={() => { setOptions(prev => ({ ...prev, model: model })) }}
+                        >
+                          <Check className={cn("mr-2 h-4 w-4", model.label === options.model.label ? "opacity-100" : "opacity-0")} />
+                          {
+                            model.label
+                          }
+                        </DropdownMenuItem>
+                      ))}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
                 </div>
-              </RadioGroup>
+                {
+                  [MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(value) => setOptions(prev => ({
+                        ...prev,
+                        [name]: value
+                      }))}
+                    >
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      <div className='mt-3 space-y-4 '>
+                        {
+                          selectableOptions.map(option => (
+                            <Radio
+                              key={option.value}
+                              value={option}
+                              className={({ focus, checked }) => cn("relative block cursor-pointer rounded-lg  bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between", {
+                                "border-primary": focus || checked
+                              })}
+                            >
+                              <span className='flex items-center'>
+                                <span className='flex flex-col text-xs'>
+                                  <RadioLabel as='span' className='font-medium text-gray-900'>
+                                    {option.label}
+                                  </RadioLabel>
+                                  {
+                                    option.description && (
+                                      <Description
+                                        as='span'
+                                        className='text-gray-500'
+                                      >
+                                        <span className='block sm:inline'>
+                                          {option.description}
+                                        </span>
+                                      </Description>
+                                    )
+                                  }
+                                </span>
+                              </span>
+                              <Description as="span" className="mt-2 flex text-sm  sm:ml-4 sm:mt-0 sm:flex-col sm:text-right">
+                                <span className='font-medium text-gray-9000'>
+                                  {formatPrice(option.price / 100)}
+                                </span>
+                              </Description>
+                            </Radio>
+                          ))
+                        }
+                      </div>
+                    </RadioGroup>
+                  ))
+                }
+              </div>
             </div>
           </div>
-
         </ScrollArea>
+        <div className='w-full px-8 h-16 bg-white'>
+          <div className='h-px w-full bg-zinc-200' />
+          <div className='w-full h-full flex  justify-end items-center'>
+            <div className='w-full flex gap-6 items-center'>
+              <p className='font-medium whitespace-nowrap'>
+                {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
+              </p>
+              <Button size="sm" className='w-full'>
+                Continue <ArrowRight className='w-4 h-4 ml-1.5 inline'/>
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div >
   )
