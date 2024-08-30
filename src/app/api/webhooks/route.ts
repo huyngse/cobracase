70b569193b205ee8de139c456ad4f30f00002729
huyngse <ngogiahuy775309@gmail.com>
2024-08-30 20:50:13 +0700

    Sending thank you email

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from 'resend';
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
     try {
@@ -26,7 +30,7 @@ export async function POST(req: NextRequest) {
             }
             const billingAddress = session.customer_details!.address
             const shippingAddress = session.shipping_details!.address
-            await db.order.update({
+            const updatedOrder = await db.order.update({
                 where: {
                     id: orderId
                 },
@@ -54,7 +58,27 @@ export async function POST(req: NextRequest) {
                     }
                 }
             })
+            
+            await resend.emails.send({
+                from: "CaseCobra <ngogiahuy775309@gmail.com>",
+                to: [event.data.object.customer_details.email],
+                subject: "Thanks for your order!",
+                react: OrderReceivedEmail({
+                    orderId: orderId,
+                    orderDate: updatedOrder.createdAt.toLocaleString(),
+                    //  @ts-ignore
+                    shippingAddress: {
+                        name: session.customer_details?.name!,
+                        city: shippingAddress?.city!,
+                        country: shippingAddress?.country!,
+                        postalCode: shippingAddress?.postal_code!,
+                        street: shippingAddress?.line1!,
+                        state: shippingAddress!.state
+                    }
+                })
+            })
         }
+
         return NextResponse.json({
             result: event,
             ok: true
