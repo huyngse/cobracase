2336ddac042cd8049264a8875c2160d7edf5e8c7
huyngse <ngogiahuy775309@gmail.com>
2024-08-30 08:13:34 +0700

    Fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  "The case feels durable and I even got a compliment on the design. Had the case for two and a half month now and <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had before, the image started faded into yellow-ish color after a couple weeks. Love it."
+                  &quot;The case feels durable and I even got a compliment on the design. Had the case for two and a half month now and <span className="p-0.5 bg-slate-800 text-white">the image is super clear</span>, on the case I had before, the image started faded into yellow-ish color after a couple weeks. Love it.&quot;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
@@ -142,7 +142,7 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>
-                  "I usually keep my phone together with my key in my pocket and that led to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>. I dig it."
+                  &quot;I usually keep my phone together with my key in my pocket and that led to some pretty heavy scratchmarks on all of my last phone cases. This one, besides a barely noticeable scratch on the corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>. I dig it.&quot;
                 </p>
               </div>
               <div className="flex gap-4 mt-2">
