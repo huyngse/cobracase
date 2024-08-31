61aa425eac56cd2c5b532bd53a89bde0120e28f1
huyngse <ngogiahuy775309@gmail.com>
2024-08-31 09:12:48 +0700

    Metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,34 @@ export const formatPrice = (price: number) => {
     currency: "USD",
   });
   return formatter.format(price);
+}
+
+export function constructMetadata({
+  title = "CaseCobra - Custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in seconds",
+  image = "/thumbnail.png",
+  icon = "/icon.ico"
+}: {
+  title?: string,
+  description?: string,
+  image?: string,
+  icon?: string,
+} = {}): Metadata {
+  return {
+    metadataBase: new URL('https://cobracase-5oop.vercel.app/'),
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [{ url: image }]
+    },
+    icons: icon,
+  }
 }
